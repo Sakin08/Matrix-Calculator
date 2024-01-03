8c340e86e513b9274f7e6a698d929b858796ab7f
Sakin08 <138347254+Sakin08@users.noreply.github.com>
2024-01-03 22:59:28 +0600

    added the multiplication function

--- main.cpp
+++ main.cpp
@@ -44,6 +44,31 @@ vector<vector<int>> subtractMatrices(const vector<vector<int>>& matrix1, const v
     return result;
 }
 
+vector<vector<int>> multiplyMatrices(const vector<vector<int>>& matrix1, const vector<vector<int>>& matrix2) {
+    int rows1 = matrix1.size();
+    int cols1 = matrix1[0].size();
+    int rows2 = matrix2.size();
+    int cols2 = matrix2[0].size();
+
+    if (cols1 != rows2) {
+        cout << "Invalid matrix multiplication: Number of columns in matrix1 must be equal to the number of rows in matrix2." << endl;
+        return {};
+    }
+
+    vector<vector<int>> result(rows1, vector<int>(cols2, 0));
+
+    for (int i = 0; i < rows1; ++i) {
+        for (int j = 0; j < cols2; ++j) {
+            for (int k = 0; k < cols1; ++k) {
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+
+    return result;
+}
+
+
 using namespace std;
 int main() {
 
