0d2679102f1773cb2b60e2a86352a8698cf342dd
Sakin08 <138347254+Sakin08@users.noreply.github.com>
2024-01-03 22:56:02 +0600

    added the matrix addition function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <vector>
 
+vector<vector<int>> addMatrices(const vector<vector<int>>& matrix1, const vector<vector<int>>& matrix2) {
+    int rows1 = matrix1.size();
+    int cols1 = matrix1[0].size();
+    int rows2 = matrix2.size();
+    int cols2 = matrix2[0].size();
+
+    if (rows1 != rows2 || cols1 != cols2) {
+        cout << "Invalid matrix addition: Matrices must have the same dimensions." << endl;
+        return {};
+    }
+
 using namespace std;
 int main() {
+
   
       return 0;
 }
