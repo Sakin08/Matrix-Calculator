d591bed533e7ab7cc162f259567afe4e0c8993c3
Sakin08 <138347254+Sakin08@users.noreply.github.com>
2024-01-03 22:58:33 +0600

    sunstract function of matrix

--- main.cpp
+++ main.cpp
@@ -22,6 +22,28 @@ vector<vector<int>> addMatrices(const vector<vector<int>>& matrix1, const vector
     return result;
 }
 
+vector<vector<int>> subtractMatrices(const vector<vector<int>>& matrix1, const vector<vector<int>>& matrix2) {
+    int rows1 = matrix1.size();
+    int cols1 = matrix1[0].size();
+    int rows2 = matrix2.size();
+    int cols2 = matrix2[0].size();
+
+    if (rows1 != rows2 || cols1 != cols2) {
+        cout << "Invalid matrix subtraction: Matrices must have the same dimensions." << endl;
+        return {};
+    }
+
+    vector<vector<int>> result(rows1, vector<int>(cols1, 0));
+
+    for (int i = 0; i < rows1; ++i) {
+        for (int j = 0; j < cols1; ++j) {
+            result[i][j] = matrix1[i][j] - matrix2[i][j];
+        }
+    }
+
+    return result;
+}
+
 using namespace std;
 int main() {
 
