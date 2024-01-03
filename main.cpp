deb9c141f77ff827d839f2c357f4c53b49f9a67f
Sakin08 <138347254+Sakin08@users.noreply.github.com>
2024-01-03 23:07:22 +0600

    added multiple by a scaller function

--- main.cpp
+++ main.cpp
@@ -247,6 +247,21 @@ int matrixTrace(const std::vector<std::vector<int>>& matrix) {
     return trace;
 }
 
+//  multiply by a scalar
+vector<vector<int>> multiplyMatrixByScalar(const vector<vector<int>>& matrix, int n) {
+
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+
+    vector<vector<int>> result(rows, vector<int>(cols, 0));
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            result[i][j] = matrix[i][j] * n;
+        }
+    }
+
+    return result;
+}
 
 using namespace std;
 int main() {
